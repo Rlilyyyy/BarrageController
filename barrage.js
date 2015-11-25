bedcb2c48a6898d866b367cfd457ba84dd6ec177
BingoLee <bingohlee@gmail.com>
2015-11-25 16:27:11 +0800

    将一些函数变为prototype的共用函数

--- barrage.js
+++ barrage.js
@@ -2,11 +2,11 @@ function BarrageController(info) {
 
 	'use strict';
 
-	var barrageElem = document.getElementById(info.elemName);
+	this.barrageElem = document.getElementById(info.elemName);
 	// 弹幕容器高度
-	var elemHeight = barrageElem.offsetHeight;
+	var elemHeight = this.barrageElem.offsetHeight;
 	// 弹幕容器宽度
-	var elemWidth = barrageElem.offsetWidth;
+	var elemWidth = this.barrageElem.offsetWidth;
 	// 每一条弹幕top增量
 	var topDefault = 50;
 	// 弹幕容器垂直允许的弹幕数量
@@ -24,30 +24,85 @@ function BarrageController(info) {
 	// 底部居中弹幕垂直位置
 	var bottomPosition = -1;
 	// 弹幕发送人员身份
-	var sender = {"ADMIN":"#c66b0c","VIP":"#ff0512","NORMAL":"black"};
+	this.sender = {"ADMIN":"#c66b0c","VIP":"#ff0512","NORMAL":"black"};
 	// 弹幕类型
-	var barrageItemClass = {"NORMAL":"barrageItem","TOP":"barrageItem topBarrage","BOTTOM":"barrageItem topBarrage",0:"barrageItem",1:"barrageItem topBarrage",2:"barrageItem topBarrage"};
+	this.barrageItemClass = {"NORMAL":"barrageItem","TOP":"barrageItem topBarrage","BOTTOM":"barrageItem topBarrage",0:"barrageItem",1:"barrageItem topBarrage",2:"barrageItem topBarrage"};
 	// 弹幕顺序初始化旗帜
-	var clear = false;
+	this.clear = false;
 	// 弹幕顺序初始化时间间隔
 	this.clearTime = 2000;
 	// 让不同作用域中都能访问到这个作用域
-	var that = this;
+	var self = this;
 
 	// 如果2000ms的时间内没有新的弹幕，则弹幕从头开始出现
 	function clearTopIndex() {
-		if(clear) {
+		console.log(self.clear);
+		if(self.clear) {
 			topIndex = -1;
 		}else {
-			clear = true;
+			self.clear = true;
 		}
-		setTimeout(clearTopIndex,that.clearTime);
+		setTimeout(clearTopIndex,self.clearTime);
 	}
 
 	setTimeout(clearTopIndex,this.clearTime);
 
+
+	// 弹幕动画结束移除
+	function handler() {
+		console.log(this)
+		self.barrageElem.removeChild(this);
+		this.removeEventListener("webkitTransitionEnd",handler,false);
+	}
+
+	// 添加普通弹幕
+	this.appendBarrage = function(barrage) {
+		var newElem = self.createBarrageItemElem(barrage);
+		(topIndex >= 0 && topIndex <= topMax-2)?topIndex++:topIndex=0;
+		newElem.style.top = topIndex % topMax * topDefault + "px";
+		duration = parseInt(Math.random() * durationRange) + durationMin;
+		// duration < durationMin?duration = durationMax:duration -= 2;
+		
+		newElem.style.transitionDuration = duration + "s";
+		self.barrageElem.appendChild(newElem);
+		
+		setTimeout(function() {
+			newElem.className += " barrageAnimation";
+			newElem.addEventListener("webkitTransitionEnd",handler,false);
+		},100);
+	}
+
+	// 添加顶部居中弹幕
+	this.appendTopBarrage = function(barrage) {
+		var newElem = self.createBarrageItemElem(barrage);
+		console.log(newElem);
+		(topPosition >=0 && topPosition <= topMax-2)?topPosition++:topPosition = 0;
+		newElem.style.top = topPosition % topMax * topDefault + "px";
+
+		self.barrageElem.appendChild(newElem);
+
+		setTimeout(function(){self.barrageElem.removeChild(newElem)},1000);
+	}
+
+	// 添加底部居中弹幕
+	this.appendBottomBarrage = function(barrage) {
+		var newElem = self.createBarrageItemElem(barrage);
+		
+		(bottomPosition >=0 && bottomPosition <= topMax-2)?bottomPosition++:bottomPosition = 0;
+		newElem.style.bottom = bottomPosition % topMax * topDefault + "px";
+
+		self.barrageElem.appendChild(newElem);
+
+		setTimeout(function(){self.barrageElem.removeChild(newElem)},1000);
+	}
+}
+
+BarrageController.prototype = {
+	
+	constructor: BarrageController,
+
 	// 创建新的弹幕，传入image为头像地址，text为弹幕内容
-	function createBarrageItemElem(barrage) {
+	createBarrageItemElem: function(barrage) {
 
 		if(!barrage.text)	return;
 
@@ -58,10 +113,10 @@ function BarrageController(info) {
 		var son2OfsonElem2 = document.createElement("span");
 
 
-		if(barrageItemClass[barrage.position]) {
-			barrageModel.className = barrageItemClass[barrage.position];
+		if(this.barrageItemClass[barrage.position]) {
+			barrageModel.className = this.barrageItemClass[barrage.position];
 		}else {
-			barrageModel.className = barrageItemClass["NORMAL"];
+			barrageModel.className = this.barrageItemClass["NORMAL"];
 		}
 		sonElem1.className = "barrageImg";
 		sonElem2.className = "barrageText";
@@ -81,10 +136,10 @@ function BarrageController(info) {
 		}
 
 		if(barrage.sender) {
-			if(sender[barrage.sender]) {
-				son2OfsonElem2.style.color = sender[barrage.sender];
+			if(this.sender[barrage.sender]) {
+				son2OfsonElem2.style.color = this.sender[barrage.sender];
 			}else {
-				son2OfsonElem2.style.color = sender["NORMAL"];
+				son2OfsonElem2.style.color = this.sender["NORMAL"];
 			}
 		}
 
@@ -95,88 +150,7 @@ function BarrageController(info) {
 		barrageModel.appendChild(sonElem2);
 
 		return barrageModel;
-	}
-
-	// 弹幕动画结束移除
-	function handler() {
-		barrageElem.removeChild(this);
-		this.removeEventListener("webkitTransitionEnd",handler,false);
-	}
-
-	// 添加普通弹幕
-	this.appendBarrage = function(barrage) {
-		var newElem = createBarrageItemElem(barrage);
-		(topIndex >= 0 && topIndex <= topMax-2)?topIndex++:topIndex=0;
-		newElem.style.top = topIndex % topMax * topDefault + "px";
-		duration = parseInt(Math.random() * durationRange) + durationMin;
-		// duration < durationMin?duration = durationMax:duration -= 2;
-		
-		newElem.style.transitionDuration = duration + "s";
-		barrageElem.appendChild(newElem);
-		
-		setTimeout(function() {
-			newElem.className += " barrageAnimation";
-			newElem.addEventListener("webkitTransitionEnd",handler,false);
-		},100);
-	}
-
-	// 添加顶部居中弹幕
-	this.appendTopBarrage = function(barrage) {
-		var newElem = createBarrageItemElem(barrage);
-		console.log(newElem);
-		(topPosition >=0 && topPosition <= topMax-2)?topPosition++:topPosition = 0;
-		newElem.style.top = topPosition % topMax * topDefault + "px";
-
-		barrageElem.appendChild(newElem);
-
-		setTimeout(function(){barrageElem.removeChild(newElem)},1000);
-	}
-
-	// 添加底部居中弹幕
-	this.appendBottomBarrage = function(barrage) {
-		var newElem = createBarrageItemElem(barrage);
-		
-		(bottomPosition >=0 && bottomPosition <= topMax-2)?bottomPosition++:bottomPosition = 0;
-		newElem.style.bottom = bottomPosition % topMax * topDefault + "px";
-
-		barrageElem.appendChild(newElem);
-
-		setTimeout(function(){barrageElem.removeChild(newElem)},1000);
-	}
-
-	// return {
-	// 	addBarrage: function(barrage) {
-	// 		clear = false;
-	// 		switch(barrage.position) {
-	// 			case "NORMAL":
-	// 			case 0:
-	// 				appendBarrage(barrage);
-	// 				break;
-	// 			case "TOP":
-	// 			case 1:
-	// 				appendTopBarrage(barrage);
-	// 				break;
-	// 			case "BOTTOM":
-	// 			case 2:
-	// 				appendBottomBarrage(barrage);
-	// 				break;
-	// 			default:
-	// 				appendBarrage(barrage);
-	// 				break;
-	// 		}
-			
-	// 	},
-	// 	setClearTime: function(newTime) {
-	// 		if(newTime > 0 && newTime) {
-	// 			clearTime = newTime;
-	// 		}
-	// 	}
-	// };
-}
-
-BarrageController.prototype = {
-	
-	constructor: BarrageController,
+	},
 
 	setClearTime: function(newTime) {
 		if(newTime > 0 && newTime) {
@@ -185,6 +159,7 @@ BarrageController.prototype = {
 	},
 
 	addBarrage: function(barrage) {
+		console.log(this)
 		this.clear = false;
 		switch(barrage.position) {
 			case "NORMAL":
